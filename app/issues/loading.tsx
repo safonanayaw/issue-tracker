3416daf556ceac91c4427bcc994c0f63e091382b
safonanayaw <safoasare17@gmail.com>
2025-11-29 03:50:33 +0000

    Add loading skeleton

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,19 +1,16 @@
-import React from "react";
-import { Button, Table } from '@radix-ui/themes'
-import Link from "next/link";
-import { prisma } from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from 'delay';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from "./IssueAction";
 
 
-const IssuePage = async () => {
-    const issues = await prisma.issues.findMany();
-    await delay(2000);
+const loading = () => {
+    const issues = [1,2,3,4,5]
     return (
         <div>
-            <IssueActions/>
-            <Table.Root variant="surface">
+        <IssueActions />
+        <Table.Root variant="surface">
                 <Table.Header>
                     <Table.Row>
                         <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
@@ -23,20 +20,19 @@ const IssuePage = async () => {
                 </Table.Header>
                 <Table.Body>
                         {issues.map(issue => (
-                            <Table.Row key={issue.id}>
+                            <Table.Row key={issue}>
                                 <Table.Cell>
-                                    {issue.title}
-                                <div className="block md:hidden"><IssueStatusBadge status={issue.status} /></div>
+                                    <Skeleton />
+                                <div className="block md:hidden"><Skeleton /></div>
                                 </Table.Cell>
-                                <Table.Cell className="hidden md:table-cell"><IssueStatusBadge status={issue.status} /></Table.Cell>
-                                <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+                                <Table.Cell className="hidden md:table-cell"><Skeleton /></Table.Cell>
+                                <Table.Cell className="hidden md:table-cell"><Skeleton /></Table.Cell>
                             </Table.Row>
                         ))} 
                 </Table.Body>
             </Table.Root>
         </div>
-        
     )
 }
 
-export default IssuePage;
+export default loading;
