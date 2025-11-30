e8d15fe5bcf6da737f38182975f5ad82e12c81b7
safonanayaw <safoasare17@gmail.com>
2025-11-30 15:40:26 +0000

    Add additional loading skeleton

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import { Card, Heading, Text } from '@radix-ui/themes';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import ReactMarkdown from 'react-markdown';
+import delay from 'delay';
 
 interface Props {
     params: {id: string}
@@ -16,11 +17,12 @@ const IssuePageDetail = async ({params} : Props) => {
     });
     if(!issue)
         notFound();
+    await delay(2000)
     return (
         <div>
             
             <Heading>{issue.title}</Heading>
-            <div className='flex space'>
+            <div className='flex space my-2'>
             <IssueStatusBadge status={issue.status} />
             <Text>{issue.createdAt.toDateString()}</Text>
             </div>
