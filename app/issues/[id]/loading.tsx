63205be1a61117f53828e10834629aaaf670cdea
safonanayaw <safoasare17@gmail.com>
2025-11-30 15:40:26 +0000

    Add additional loading skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,22 @@
+import { Heading, Card, Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css';
+
 const loading = () => {
+    const issues = [1,2,3,4,5];
     return(
-        <div>
-            Loading....
-        </div>
+        <Box>
+            <Skeleton className="max-w-xl"/>
+            <div className='flex space-x-3'>
+            <Skeleton  width="5rem"/>
+            <Skeleton width="8rem"/>
+            </div>
+
+            <Card className="prose" mt="4">
+                <Skeleton count={3}/>
+            </Card>
+
+        </Box>
     )
 }
 export default loading;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,14 @@
+import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css';
+
 const loading = () => {
     return(
         <div>
-            Loading....
+            <Box className="max-w-xl">
+                <Skeleton />
+                <Skeleton height="20rem"/>
+            </Box>
         </div>
     )
 }
